cfe2ba341357a4c424ef00f3b2dae1a0b258f285
Marco Santonastasi <marco.santonastasi@gmail.com>
2022-01-17 23:00:18 +0100

    Update 01-exe-js_functions.js

--- execises/01-exe-js_functions.js
+++ execises/01-exe-js_functions.js
@@ -1,1 +1,12 @@
 // Practice
+
+/**
+ * Create a function doTheMath(num1, sign, num2)
+ * that will return the result of the mathematical operations of num1 operator num2.
+ * Allowed operators (signs) are +, -, *, /, %, **.
+ * Hint: you can use the switch statement.
+ */
+
+function doTheMath(){
+    // your code here ...
+}
